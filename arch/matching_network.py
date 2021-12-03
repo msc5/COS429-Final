15ded55547269bb0275d5ce28713690a79971f46
msc5 <19colemanm@gmail.com>
2021-12-03 00:27:59 -0500

    thurs night matt

---
+++ arch/matching_network.py
@@ -0,0 +1,8 @@
+
+import torch
+import torch.nn as nn
+
+from torchsummary import summary
+
+from .relation_network import ConvBlock, PoolBlock
+
--- arch/resnet.py
+++ arch/resnet.py
@@ -3,6 +3,26 @@ import torch.nn as nn
 
 from torchsummary import summary
 
+class Conv(nn.Module):
+
+    def __init__(self, fi, fo, fs, skip, stride=None, padding=None):
+        super(Conv, self).__init__()
+        self.skip = skip
+        conv = nn.Conv2d(fi, fo, fs, padding='same')
+        if stride is not None:
+            conv = nn.Conv2d(fi, fo, fs, stride=stride, padding=padding)
+        self.stack = nn.Sequential(
+                conv,
+                nn.BatchNorm2d(fo),
+                nn.ReLU()
+                )
+        
+    def forward(self, x):
+        res = x
+        x = self.stack(x)
+        if not self.skip: x = x + res
+        return x
+
 
 class ConvBlock(nn.Module):
 
@@ -10,9 +30,9 @@ class ConvBlock(nn.Module):
         super(ConvBlock, self).__init__()
         self.skip = skip
         self.stack = nn.Sequential(
-            nn.Conv2d(fi, fo, fs, padding='same'),
+            Conv(fi, fo, fs, skip),
             nn.ReLU(),
-            nn.Conv2d(fo, fo, fs, padding='same')
+            Conv(fo, fo, fs, False),
         )
 
     def forward(self, x):
@@ -29,6 +49,7 @@ class ConvStack(nn.Module):
         super(ConvStack, self).__init__()
         self.ent = ConvBlock(fi, fo, 3, skip)
         list = nn.ModuleList([ConvBlock(fo, fo, 3, False) for _ in range(nb)])
+        list.append(nn.MaxPool2d(2, ceil_mode=True))
         self.stack = nn.Sequential(*list)
 
     def forward(self, x):
@@ -42,20 +63,16 @@ class ResNet(nn.Module):
     def __init__(self, fi, hi, wi, lo):
         super(ResNet, self).__init__()
         fo = 64
-        # ResNet34
         self.stack = nn.Sequential(
-            nn.Conv2d(fi, fo // 2, 7, padding='same'),
-            nn.MaxPool2d(2),
-            ConvStack(fo // 2, fo * 2**0, 3, True),
+            Conv(fi, fo, 7, True, stride=2, padding=3),
+            nn.MaxPool2d(2, ceil_mode=True),
+            ConvStack(fo, fo * 2**0, 3, True),
             ConvStack(fo * 2**0, fo * 2**1, 4, True),
             ConvStack(fo * 2**1, fo * 2**2, 6, True),
             ConvStack(fo * 2**2, fo * 2**3, 3, True),
-            nn.AvgPool2d(2)
+            nn.AdaptiveAvgPool2d(1)
         )
-        ho = hi // 2 // 2
-        wo = wi // 2 // 2
-        self.li = fo * 2**3 * ho * wo
-        self.dec = nn.Linear(self.li, lo)
+        self.dec = nn.Linear(fo * 2**3, lo)
 
     def forward(self, x):
         x = self.stack(x)
@@ -67,5 +84,9 @@ class ResNet(nn.Module):
 if __name__ == '__main__':
     hi = 105
     wi = 105
-    model = ResNet(3, hi, wi, 1000)
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    model = ResNet(3, hi, wi, 1000).to(device)
+    actual = torch.hub.load('pytorch/vision:v0.10.0', 'resnet34', pretrained=True).to(device)
     summary(model, input_size=(3, hi, wi))
+    summary(actual, input_size=(3, hi, wi))
+
